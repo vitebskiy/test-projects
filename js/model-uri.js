0b73ece6e4759e9a9a9123be992cc37dca6db854
Mikhail Vitebskiy <mihan1993@inbox.ru>
2025-09-22 22:12:54 +0300

    Fix model-uri.js with proper GLB format

--- js/model-uri.js
+++ js/model-uri.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a99f2fcc3c6d90f1baeca0f423ad0af7d6e040b19b0275ffd6d6eef70d292faa
-size 428412716
+oid sha256:e13753a6037e27ebda7ed27dee5974d59c143c01726b19c9a2e24019b6f7e9f5
+size 428412709
