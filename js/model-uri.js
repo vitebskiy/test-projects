a9ad7a9c2b0020ff84c0bb7776ee62b80f2c541b
Mikhail Vitebskiy <mihan1993@inbox.ru>
2025-09-22 21:55:35 +0300

    Fix model-uri.js syntax

--- js/model-uri.js
+++ js/model-uri.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43d51c0b94a0d7bec6ed9528f5ce976f2e45ae3a337167c5ecf1d5b7c41d4765
-size 428412722
+oid sha256:a99f2fcc3c6d90f1baeca0f423ad0af7d6e040b19b0275ffd6d6eef70d292faa
+size 428412716
